859663a79c5a7eec3caa9cd2c5d30a2e9fb53144
Lucas Borges de Almeida <lucas_borges06@hotmail.com>
2022-05-11 21:26:50 -0300

    commit inicial

---
+++ CalcularMedia.py
@@ -0,0 +1,6 @@
+x = int(input("Digite o primeiro valor: "));
+y = int(input("Digite o segundo valor: "));
+
+media = (x + y) / 2;
+
+print("\nA média aritmética entre {} e {} é {:.1f}\n".format(x, y, media));
